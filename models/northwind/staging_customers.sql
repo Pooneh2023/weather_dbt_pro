412416c0574552c376173c992773e73036b11a66
Pooneh2023 <pooneh.nezamabadi1990@gmail.com>
2026-04-20 14:48:51 +0200

    feat: staging_customers.sql

---
+++ models/northwind/staging_customers.sql
@@ -0,0 +1,26 @@
+WITH phone_cleaning AS (
+    SELECT
+        customer_id,
+        company_name,
+        contact_name,
+        contact_title,
+        country,
+        region,
+        city,
+        postal_code,
+        CASE
+            WHEN phone LIKE '00%' THEN 
+                '+' || SUBSTRING(REGEXP_REPLACE(phone, '[^0-9]', '', 'g') FROM 3)
+            ELSE 
+                REGEXP_REPLACE(phone, '[^0-9]', '', 'g')
+        END AS phone_clean,
+                CASE
+            WHEN fax LIKE '00%' THEN 
+                '+' || SUBSTRING(REGEXP_REPLACE(fax, '[^0-9]', '', 'g') FROM 3)
+            ELSE 
+                REGEXP_REPLACE(fax, '[^0-9]', '', 'g')
+        END AS fax_clean
+    FROM {{source('north_data','customers')}}
+)
+SELECT *
+FROM phone_cleaning
